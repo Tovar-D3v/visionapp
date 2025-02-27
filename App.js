f8c62e831988219e91d8e3ad1686f5a848903406
Wilfred Tovar <w.tovar@utp.edu.co>
2025-02-27 17:31:21 -0500

    Actualizar el título de la aplicación a "Vision App I"

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Vision App</Text>
+      <Text>Vision App I</Text>
       <StatusBar style="auto" />
     </View>
   );
